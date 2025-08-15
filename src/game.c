f4f0b3af5c9487d19d92a18c50119076226cf013
Harrison White <harrisonkwhite@gmail.com>
2025-08-15 19:43:10 +1000

    wowowow

--- src/game.c
+++ src/game.c
@@ -1,13 +1,39 @@
 #include "game.h"
 
+typedef enum {
+    ek_tilemap_room_type_extra,
+    ek_tilemap_room_type_left_right_exits,
+    ek_tilemap_room_type_left_right_top_exits,
+    ek_tilemap_room_type_left_right_bottom_exits,
+    ek_tilemap_room_type_left_right_bottom_top_exits
+} e_tilemap_room_type;
+
+static bool WARN_UNUSED_RESULT GenTilemap(s_tilemap* const tm) {
+    assert(IS_ZERO(*tm));
+
+    return true;
+}
+
 bool InitGame(const s_game_init_context* const zfw_context) {
     s_game* const game = zfw_context->dev_mem;
+
+    if (!GenTilemap(&game->lvl.tilemap)) {
+        return false;
+    }
 
     return true;
 }
 
 e_game_tick_result GameTick(const s_game_tick_context* const zfw_context) {
     s_game* const game = zfw_context->dev_mem;
+
+    if (IsKeyPressed(&zfw_context->input_context, ek_key_code_space)) {
+        ZERO_OUT(game->lvl.tilemap);
+
+        if (!GenTilemap(&game->lvl.tilemap)) {
+            return false;
+        }
+    }
 
     return ek_game_tick_result_normal;
 }
--- src/game.h
+++ src/game.h
@@ -3,8 +3,28 @@
 
 #include <zfwc.h>
 
+#define TILEMAP_ROOM_WIDTH 12
+#define TILEMAP_ROOM_HEIGHT 8
+#define TILEMAP_ROOMS_HOR 4
+#define TILEMAP_ROOMS_VERT 4
+#define TILEMAP_WIDTH (TILEMAP_ROOM_WIDTH * TILEMAP_ROOMS_HOR)
+#define TILEMAP_HEIGHT (TILEMAP_ROOM_HEIGHT * TILEMAP_ROOMS_VERT)
+
+typedef enum {
+    ek_tile_state_empty,
+    ek_tile_state_dirt
+} e_tile_state;
+
 typedef struct {
-    int a;
+    e_tile_state states[TILEMAP_HEIGHT][TILEMAP_WIDTH];
+} s_tilemap;
+
+typedef struct {
+    s_tilemap tilemap;
+} s_level;
+
+typedef struct {
+    s_level lvl;
 } s_game;
 
 bool InitGame(const s_game_init_context* const zfw_context);
